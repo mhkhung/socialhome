86a43443f8b78ba9f32af4f2e017e720644a2294
Michael Hung <mhkhung@gmail.com>
2023-09-10 23:20:52 -0400

    Add data source

--- src/views/index-sections/SectionCarousel.js
+++ src/views/index-sections/SectionCarousel.js
@@ -201,7 +201,9 @@ function SectionCarousel() {
               </Card>
             </Col>
           </Row>
-          <a href="https://api.hongkongers.net/ppdb">{t('ppdb_source')}</a>
+          <a href="https://api.hongkongers.net/ppdb" target="_blank" rel="noreferrer">{t('ppdb_source')}</a>
+          <p>{t('data_source')} <a href="https://www.hongkongwatch.org/" target="_blank" rel="noreferrer">{t('hongkongwatch')}</a> <a href="https://www.thecfhk.org" target="_blank" rel="noreferrer">{t('thecfhk')}</a> {t('and_others')}
+          </p>
         </Container>
       </div>{" "}
     </>
